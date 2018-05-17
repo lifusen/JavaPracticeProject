020f48abb869e0df0bdd0c98b355e6303d812ce2
李富森 <570429601@qq.com>
2018-05-17 14:17:55 +0800

    这个项目重新提交

--- SSH练习项目lfs/src/com/lifusen/www/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.lifusen.www.service;
-
-import com.lifusen.www.entity.User;
-
-public interface UserService {
-	User login(User u);
-	User regist(User u);
-	User sele(String loginName);
-}
