a3190e41237dd7c247a7315051885fb6e5070284
李富森 <570429601@qq.com>
2018-05-24 23:14:35 +0800

    修改天气调用接口

--- SSM练习项目lfs1/src/main/java/com/lfs/www/web/controller/CommonController.java
+++ SSM练习项目lfs1/src/main/java/com/lfs/www/web/controller/CommonController.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.lfs.www.util.WeatherInfo;
+
 import cn.com.webxml.Wsweather;
 
 @Controller
@@ -17,7 +19,8 @@ public class CommonController {
 	
 	@RequestMapping("")
 	public String toIndex(HttpServletRequest request,Model model){
-		List<String> list = Wsweather.getWeather("成都");
+		//List<String> list = Wsweather.getWeather("成都");
+		String list = WeatherInfo.GetWeatherData("成都");
 		model.addAttribute("weather",list);
 		return "common/index";
 	}
